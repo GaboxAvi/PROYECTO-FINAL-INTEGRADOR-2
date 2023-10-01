7817a26c073d021e3ae7619aa298f56c1d50f84b
Gabriel José Herrera Ávila <gabriel.herrera20125@gmail.com>
2023-10-01 12:28:19 -0300

    Añadido la interacción de los objetos desde JS a html. Inicio del código css.

--- js/campeonObjeto.js
+++ js/campeonObjeto.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
   // Cargar campeones.json
   fetch("json/campeones.json")
     .then((response) => response.json())
@@ -8,18 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const campeonCard = document.getElementById("campeones-list");
 
-      localStorage.setItem("campeonesDescripcion",JSON.stringify(campeones));
-      
+      localStorage.setItem("campeonesDescripcion", JSON.stringify(campeones));
+
       campeones.forEach((campeon) => {
-        const htmlContent = `
+
+        const htmlContentCampeon = `
           <div class="campeon-card">
             <img src="${campeon.imagen}" alt="${campeon.nombre}">
             <h2>${campeon.nombre}</h2>
             <a href="./descripcion.html?id=${campeon.id}" class="ver-mas-boton">Ver más</a>
           </div>
           `;
-        
-        campeonCard.innerHTML += htmlContent;
+
+        campeonCard.innerHTML += htmlContentCampeon;
       });
     })
 
@@ -27,66 +29,33 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error("Error al cargar campeones.json", error);
     });
 
-  // Cargar objetos.json
-  // fetch("json/objetos.json")
-  //   .then((response) => response.json())
-  //   .then((objetosJSON) => {
-  //     const objetos = objetosJSON.objetos;
+  ``;
 
-  //     const objetosList = document.getElementById("objetos-list");
+  //Cargar objetos.json
+  fetch("json/objetos.json")
+    .then((response) => response.json())
+    .then((objetosJSON) => {
+      const objetos = objetosJSON.objetos;
 
-  //     objetos.forEach((objeto) => {
-  //       const objetoCard = document.createElement("div");
-  //       objetoCard.classList.add("objeto-card");
+      const objetoCard = document.getElementById("objetos-list");
 
-  //       const objetoImagen = document.createElement("img");
-  //       objetoImagen.src = objeto.imagen;
-  //       objetoImagen.alt = objeto.nombre;
-  //       objetoCard.appendChild(objetoImagen);
+      localStorage.setItem("objetosDescripcion", JSON.stringify(objetos))
 
-  //       const objetoNombre = document.createElement("h2");
-  //       objetoNombre.textContent = objeto.nombre;
-  //       objetoCard.appendChild(objetoNombre);
+      objetos.forEach((objeto) => {
 
-  //       const verMasButton = document.createElement("button");
-  //       verMasButton.textContent = "Ver más";
-  //       objetoCard.appendChild(verMasButton);
+        const htmlContentObjeto = `
+          <div class="objeto-card">
+            <img src="${objeto.imagen}" alt="${objeto.nombre}">
+            <h2>${objeto.nombre}</h2>
+            <a href="./descripcion.html?id=${objeto.id}" class="ver-mas-boton">Ver más</a>
+          </div>
+        `;
 
-  //       const objetoPrecio = document.createElement("h4");
-  //       objetoPrecio.classList.add("objeto-precio");
-  //       objetoCard.appendChild(objetoPrecio);
-
-  //       const objetoAtributos = document.createElement("h5");
-  //       objetoAtributos.classList.add("objeto-atributos");
-  //       objetoCard.appendChild(objetoAtributos);
-
-  //       const objetoHistoria = document.createElement("p");
-  //       objetoHistoria.classList.add("objeto-historia");
-  //       objetoCard.appendChild(objetoHistoria);
-
-  //       verMasButton.addEventListener("click", () => {
-  //         objetoPrecio.textContent = objeto.precio;
-  //         objetoAtributos.textContent = objeto.atributos;
-  //         objetoHistoria.textContent = objeto.historia;
-
-  //         // Agregar un botón para ocultar la historia
-  //         const ocultarButton = document.createElement("button");
-  //         ocultarButton.textContent = "Ocultar";
-  //         objetoCard.appendChild(ocultarButton);
-
-  //         // Agregar un evento de clic al botón "Ocultar" para esconder la historia
-  //         ocultarButton.addEventListener("click", () => {
-  //           objetoPrecio.textContent = "";
-  //           objetoAtributos.textContent = "";
-  //           objetoHistoria.textContent = "";
-  //           objetoCard.removeChild(ocultarButton);
-  //         });
-  //       });
-
-  //       objetosList.appendChild(objetoCard);
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error al cargar objetos.json", error);
-  //   });
+        objetoCard.innerHTML += htmlContentObjeto;
+      });
+    })
+    .catch((error) => {
+      console.error("Error al cargar objetos.json", error);
+    });
 });
+``;
--- js/descripcion.js
+++ js/descripcion.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function (){
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get("id");
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
 
-    const campeones = JSON.parse(localStorage.getItem("campeonesDescripcion"));
+  const campeones = JSON.parse(localStorage.getItem("campeonesDescripcion"));
+  const selectCampeon = campeones.find((campeon) => campeon.id == id);
 
-    const selectCampeon = campeones.find((campeon) => campeon.id == id);
-    if(selectCampeon){
+  const objetos = JSON.parse(localStorage.getItem("objetosDescripcion"));
+  const selectObjeto = objetos.find((objeto) => objeto.id == id);
 
-        console.log("Campeón seleccionado:", selectCampeon);
-        
-        const htmlDescripcion = `
+  if (selectCampeon) {
+    const htmlDescripcionCampeon = `
             <div class="campeon-card">
                 <h1>${selectCampeon.nombre}</h1>
                 <h2>${selectCampeon.apodo}</h2>
@@ -25,16 +25,38 @@ document.addEventListener("DOMContentLoaded", function (){
                 <button onclick="volverIndex()" class="volver-button">Volver Atrás</button>
             </div>
         `;
-        const campeonDescripcion = document.getElementById("campeon-descripcion");
-        campeonDescripcion.innerHTML = htmlDescripcion;
 
-    } else{
-        const campeonDescripcion = document.getElementById("campeon-descripcion");
-        campeonDescripcion.innerHTML = "<p>El campeón seleccionado no ha sido encontrado.</p>"
-    }
+    const campeonDescripcion = document.querySelector("main div:first-child");
+    campeonDescripcion.classList.toggle("campeon-descripcion");
+    campeonDescripcion.innerHTML = htmlDescripcionCampeon;
+  } else if (selectObjeto) {
+    const htmlContentObjeto = `
+            <div class="campeon-card">
+                <img src="${selectObjeto.imagen}" alt="${selectObjeto.nombre}">
+                <hr>
+                <h1>${selectObjeto.nombre}</h1>
+                <h3>${selectObjeto.precio}</h3>
+        
+                <p><strong>Atributos: </strong>${selectObjeto.atributos}</p>
+                
     
+                <h2>Historia:</h2>
+                <p>${selectObjeto.historia}</p>
+    
+                <button onclick="volverIndex()" class="volver-button">Volver Atrás</button>
+            </div>
+            `;
+
+    const objetoDescripcion = document.querySelector("main div:nth-child(2)");
+    objetoDescripcion.classList.toggle("objeto-descripcion");
+    objetoDescripcion.innerHTML = htmlContentObjeto;
+  } else {
+    const campeonDescripcion = document.getElementById("campeon-descripcion");
+    campeonDescripcion.innerHTML =
+      "<p>El campeón u objeto seleccionado no ha sido encontrado.</p>";
+  }
 });
 
-function volverIndex(){
-    window.location.href = "index.html";
+function volverIndex() {
+  window.location.href = "index.html";
 }
